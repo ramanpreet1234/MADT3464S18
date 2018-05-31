a84b9b6fa7cf2855ed55139e23780ea405dae85f
ramanpreet1234 <39779096+ramanpreet1234@users.noreply.github.com>
2018-05-31 17:27:52 -0400

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,79 +6,30 @@
 
 /**
  *
- * @author Ramanpreet
+ * @author macstudent
  */
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello Java");
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=5; j++){
+                if(i==1 || j ==1 || i ==5 || j ==5)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+                }
+    }  
+             
+            }
+        
+        
+        
     
-
-int n1 = 10;
-if(n1 % 2 == 0){
-System.out.println("even");
-}else{
-System.out.println("odd");
-}
-char pass ='A';
-switch(pass){
-    case 'A':
-        System.out.println("Adult pass : valid pass");
-        break;
-    case 'S':
-         System.out.println("Student pass : valid pass");
-         break;
-    case 'O':
-         System.out.println("Senior Citizen : valid pass");
-         break;
-    default:
-         System.out.println("Invalid pass");
-         break;
-}
-char pass1 = 'A';
-switch(pass){
-    case 'A':
-    case 'S':
-    case 'O':
-        System.out.println("Valid pass");
-        break;
-    default:
-        System.out.println("Invalid pass");
-        break;
-}
-n1 = 20;
-while(n1>10){
-    System.out.println("Beyond Limit");
-    n1++;
-do{
-    System.out.println("n1:" +n1);
-    n1--;
-}while (n1>10);
-    }
-int i;
-for(i=0; i<5; i++){
-    System.out.println("i: " +i);
-}
- for(i=1; i<5; i++){   
-    for(int j=1; j<5; j++){
-        System.out.println(" * ");
-    }
-    int numbers[] = new int[5];
-    numbers[0] = 10;
-    System.out.println("number[0]: " +numbers[0]);
-    for(i=0; i<5; i++){
-        numbers[i] = (i+1);
-        System.out.println("number[" +i + "]); " +numbers[i]);
-    }
-    }
-            
-    }
-    
-}
-
-        
-        
-        
-}
-        
     
 
